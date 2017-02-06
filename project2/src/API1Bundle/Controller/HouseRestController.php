bb0026ebc8270b3fd7e9f2ce7ec892a4e0fbc96a
HienPhan <hien4687@gmail.com>
2017-02-06 10:09:04 +0700

    no message

--- project2/src/API1Bundle/Controller/HouseRestController.php
+++ project2/src/API1Bundle/Controller/HouseRestController.php
@@ -52,12 +52,12 @@ class HouseRestController extends Controller
         else if($username === NULL)
             return $formatResponse->createResponseRegister($common->RESULT_CODE_FAIL, $common->REGISTER_ADDRESS_TOKEN_ERROR_NOT_FOUND);
         else {
-            var_dump("123");die;
             if (!$valid->validationLatitude($latitude) || !$valid->validationAddress($address) ||
                 !$valid->validationLongitude($longitude) || !$valid->validationOwnername($ownername)) {
                 return $formatResponse->createResponseRegister($common->RESULT_CODE_FAIL, $common->REGISTER_ADDRESS_ERROR_INPUT);
             }
             $result = $houseLogic->getHouseInfoByUsername($latitude, $longitude, $username);
+            var_dump("123");die;
             if($result->get('Count') > 0) {
                 return $formatResponse->createResponseRegister($common->RESULT_CODE_FAIL, $common->REGISTER_ADDRESS_EXISTED);
             } else {
