447149fd72e718be38b62df081a328ba30453194
HienPhan <hien4687@gmail.com>
2017-02-06 11:57:38 +0700

    no message

--- project2/src/API1Bundle/Repository/HouseRepository.php
+++ project2/src/API1Bundle/Repository/HouseRepository.php
@@ -76,6 +76,7 @@ class HouseRepository
 
     public function newAddress($id, $username, $latitude, $longitude, $address, $ownername) {
 
+        var_dump('$latitude'); die;
         $response = $this->dynamodb->putItem([
             'TableName' => $this->tableName,
             'Item' => [
