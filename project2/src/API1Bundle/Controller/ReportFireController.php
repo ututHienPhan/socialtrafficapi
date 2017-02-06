c0b862ba1287097cbaa3cf5126f5309fb69d3ef1
HienPhan <hien4687@gmail.com>
2017-02-06 12:58:52 +0700

    no message

--- project2/src/API1Bundle/Controller/ReportFireController.php
+++ project2/src/API1Bundle/Controller/ReportFireController.php
@@ -84,9 +84,9 @@ class ReportFireController extends Controller
         $common = new Common();
         $registerResponse = new FormatResponse();
         $valid = new UserValidateHelper();
-        var_dump('100');
+
         $tokenLogic = new TokenLogic($this->get('aws.dynamodb'));
-        var_dump("123");die;
+        
         $data = $this->get('request')->getContent();
         $array = json_decode($data, true);
         $token = $array['token'];
@@ -129,7 +129,9 @@ class ReportFireController extends Controller
         $common = new Common();
         $registerResponse = new FormatResponse();
         $valid = new UserValidateHelper();
+        var_dump('100');
         $tokenLogic = new TokenLogic($this->get('aws.dynamodb'));
+        var_dump("123");die;
         $data = $this->get('request')->getContent();
         $array = json_decode($data, true);
         $token = $array["token"];
